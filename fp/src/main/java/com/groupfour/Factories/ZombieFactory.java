071055c5fddacb6b3c644508cb2b92ee567a945f
Azyur-i <107308649+Azyur-i@users.noreply.github.com>
2024-10-25 23:40:01 +0800

    redesigned zombie spawning system

--- fp/src/main/java/com/groupfour/Factories/ZombieFactory.java
+++ fp/src/main/java/com/groupfour/Factories/ZombieFactory.java
@@ -3,6 +3,7 @@ package com.groupfour.Factories;
 import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
 import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
 import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
 
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.components.AutoRotationComponent;
@@ -19,11 +20,9 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-import static com.almasb.fxgl.dsl.FXGL.*;
-
 public class ZombieFactory implements EntityFactory {
 
-    private static final int SPAWN_DISTANCE = 50;
+    private static final int SPAWN_DISTANCE = 500;
 
     private static final Point2D[] spawnPoints = new Point2D[] {
             new Point2D(SPAWN_DISTANCE, SPAWN_DISTANCE),
@@ -32,21 +31,35 @@ public class ZombieFactory implements EntityFactory {
             new Point2D(SPAWN_DISTANCE, getAppHeight() - SPAWN_DISTANCE)
     };
 
-    private static Point2D getRandomSpawnPoint() {
-        return spawnPoints[FXGLMath.random(0, 3)];
-    }
+    // just some code i needed to test
 
+    private static Point2D getRandomSpawnPoint() {
+        var players = getGameWorld().getEntitiesByType(EntityType.PLAYER);
+    
+        while (true) {
+            Point2D potentialSpawnPoint = spawnPoints[FXGLMath.random(0, spawnPoints.length - 1)];
+
+            boolean isSafe = players.stream().allMatch(player -> 
+                potentialSpawnPoint.distance(player.getPosition()) >= SPAWN_DISTANCE
+            );
+    
+            if (isSafe) {
+                return potentialSpawnPoint;
+            }
+        }
+    }
+    
     @Spawns("zombie")
     public Entity newZombie(SpawnData data) {
-    return entityBuilder(data)
-            .type(EntityType.ZOMBIE)
-            .at(getRandomSpawnPoint())
-            .with(new AutoRotationComponent().withSmoothing())
-            .collidable()
-            .viewWithBBox(new Rectangle(30, 30, Color.GREEN))
-            .with((new NetworkComponent()))
-            .with(new CellMoveComponent(40, 40, 150))
-            .with(new ZombieComponent(60))
-            .build();
-}
+        return entityBuilder(data)
+                .type(EntityType.ZOMBIE)
+                .at(getRandomSpawnPoint())
+                .with(new AutoRotationComponent().withSmoothing())
+                .collidable()
+                .viewWithBBox(new Rectangle(30, 30, Color.GREEN))
+                .with(new NetworkComponent())
+                .with(new CellMoveComponent(40, 40, 150))
+                .with(new ZombieComponent(60))
+                .build();
+    }
 }
